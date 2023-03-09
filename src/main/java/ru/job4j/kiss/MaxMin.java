b8359b06a1cdf02eb9a133071778a56cef6beccb
LESHA <belochuvach@gmail.com>
2023-03-09 22:05:46 +0300

    Принципы Kiss, Dry и Yagni [#238813]

--- src/main/java/ru/job4j/kiss/MaxMin.java
+++ src/main/java/ru/job4j/kiss/MaxMin.java
@@ -2,20 +2,21 @@ package ru.job4j.kiss;
 
 import java.util.Comparator;
 import java.util.List;
-import java.util.Set;
 import java.util.TreeSet;
 
 public class MaxMin {
 
-    public <T> T max(List<T> value, Comparator<T> comparator) {
+    private <T> TreeSet<T> find(List<T> value, Comparator<T> comparator) {
         TreeSet<T> set = new TreeSet<>(comparator);
         set.addAll(value);
-        return set.last();
+        return set;
+    }
+
+    public <T> T max(List<T> value, Comparator<T> comparator) {
+        return find(value, comparator).last();
     }
 
     public <T> T min(List<T> value, Comparator<T> comparator) {
-        TreeSet<T> set = new TreeSet<>(comparator);
-        set.addAll(value);
-        return set.first();
+        return find(value, comparator).first();
     }
 }
